332a55831bc9a5c00c79608bc793af558f26aaa1
rpehkone <pehkonenroni@gmail.com>
2023-04-29 17:19:07 +0300

    dynamic iterations

--- hypergraph.py
+++ hypergraph.py
@@ -1,6 +1,5 @@
 import networkx as nx
 import matplotlib.pyplot as plt
-import scipy #plot compiler
 import time
 import re
 
@@ -8,15 +7,19 @@ def plot_graph(edges):
 	G = nx.Graph()
 	for edge in edges:
 		G.add_edges_from([tuple(edge)])
-	# wolfram uses a proprietary force-directed graph layout algorithm
-	pos = nx.spring_layout(G, seed=42)
-	# pos = nx.spectral_layout(G)
-	nx.draw_networkx_nodes(G, pos, node_color='lightblue', node_size=500, node_shape='o', alpha=0.8)
-	nx.draw_networkx_labels(G, pos, labels={node: node for node in G.nodes()}, font_size=10)
-	nx.draw_networkx_edges(G, pos, edgelist=G.edges(), arrows=True, arrowstyle="-|>", arrowsize=20, width=1.0, edge_color='black', alpha=1)
+	if len(edges) < 40:
+		# wolfram uses a proprietary force-directed graph layout algorithm
+		# pos = nx.spectral_layout(G)
+		pos = nx.spring_layout(G, seed=42)
+		nx.draw_networkx_nodes(G, pos, node_color='lightblue', node_size=500, node_shape='o', alpha=0.8)
+		nx.draw_networkx_labels(G, pos, labels={node: node for node in G.nodes()}, font_size=10)
+		nx.draw_networkx_edges(G, pos, edgelist=G.edges(), arrows=True, arrowstyle="-|>", arrowsize=20, width=1.0, edge_color='black', alpha=1)
+	else:
+		iterations = max(1, int(80 - 0.05 * len(edges)))
+		print(f"iterations = {iterations}")
+		pos = nx.spring_layout(G, iterations=iterations)
+		nx.draw_networkx_edges(G, pos, edgelist=G.edges(), width=1.0, edge_color='black', alpha=1)
 	plt.draw()
-	plt.pause(0.1)
-	plt.clf()
 
 def _generate_substitution_function(outvars, invars):
 	# code = f"""def substitute({', '.join(input_vars)}):
@@ -101,13 +104,16 @@ def evolve_graph(initial_graph, max_steps):
 			prev_solution = current_step
 			for _ in range(current_step):
 				graph = apply_rule(graph)
+			plt.clf()
+			plot_graph(graph)
 		plt.title(f"{rule}\nSteps: {current_step}")
-		plot_graph(graph)
+		plt.pause(0.1)
 
 # https://www.wolframphysics.org/technical-introduction/basic-form-of-models/
 # 2.1 Basic structure
 # graph = [(1, 2), (1, 3), (2, 3), (4, 1)]
 # plot_graph(graph)
+# plt.show()
 
 # 2.2 First Example of a Rule
 # rule = "{{x, y}} -> {{x, y}, {y, z}}"
@@ -115,7 +121,7 @@ def evolve_graph(initial_graph, max_steps):
 
 # 2.3 A Slightly Different Rule
 # rule = "{{x, y}} -> {{z, y}, {y, x}}"
-# evolve_graph([(1, 2)], 4)
+# evolve_graph([(1, 2)], 5)
 
 # 2.4 Self-Loops
 # rule = "{{x, y}} -> {{y, z}, {z, x}}"
---
+++ readme.md
@@ -0,0 +1,7 @@
+https://www.wolframphysics.org/technical-introduction/introduction/  
+https://www.wolframphysics.org/technical-introduction/potential-relation-to-physics/potential-basic-translations/index.html  
+  
+tries to achieve same as https://github.com/maxitg/SetReplace  
+networkx graph layout algorithm very slow  
+
+<img src="gif.gif" width="500" height="auto"/>  
