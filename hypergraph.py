6a90d995ba711dee6b73bc28043c59738fc8a902
rpehkone <pehkonenroni@gmail.com>
2023-06-10 13:37:17 +0300

    gen substitution test

--- hypergraph.py
+++ hypergraph.py
@@ -20,15 +20,18 @@ def plot_graph(edges):
 		nx.draw_networkx_edges(G, pos, edgelist=G.edges(), width=1.0, edge_color='black', alpha=1)
 	plt.draw()
 
-def _generate_relation_substitution(outvars, invars):
-	code = "def relation_substition(a, b, c, d):\n"
+def _generate_substitution_test(invars, outvars):
+	code = "def substitution_test(a, b, c, d):\n"
 
 	if invars[0] == invars[1]:
-		code +=	"	if a != b:\n"+\
-				"		return [[a, b]]\n"
-		for i, v in enumerate(outvars):
-			if v == "c":
-				outvars[i] = 'd'
+		code +=	"	return a == b\n"
+	else:
+		code +=	"	return a != b\n"
+	print(code)
+	exec(code, globals())
+
+def _generate_relation_substitution(invars, outvars):
+	code = "def relation_substition(a, b, c, d):\n"
 
 	code +=	"	return ["
 	i = 0
@@ -37,20 +40,21 @@ def _generate_relation_substitution(outvars, invars):
 			i += 2
 			if i < len(outvars):
 				code += ", "
-	code += "]"
-	print(code + "\n")
+	code += "]\n"
+	print(code)
 	exec(code, globals())
 
-def _generate_graph_substitution():
+def _generate_graph_substitution(invars, outvars):
 	code = "def graph_substitution(graph):\n" +\
 	"	result = []\n" +\
 	"	max_value = max(max(p) for p in graph)\n" +\
 	"	for relation in graph:\n" +\
-	"		result += relation_substition(relation[0], relation[1], max_value + 1, max_value + 2)\n" +\
-	"		max_res = max(max(p) for p in result)\n" +\
-	"		max_value = max(max_res, max_value)\n" +\
-	"	return result"
-	print(code + "\n")
+	"		if substitution_test(relation[0], relation[1], max_value + 1, max_value + 2):\n" +\
+	"			result += relation_substition(relation[0], relation[1], max_value + 1, max_value + 2)\n" +\
+	"			max_res = max(max(p) for p in result)\n" +\
+	"			max_value = max(max_res, max_value)\n" +\
+	"	return result\n"
+	print(code)
 	exec(code, globals())
 
 def map_abc(rule):
@@ -69,9 +73,10 @@ def map_abc(rule):
 rule  = ""
 def parse_rule():
 	variables = re.findall(r"\b\w+\b", map_abc(rule))
-	input_vars, output_vars = variables[:2], variables[2:]
-	_generate_relation_substitution(output_vars, input_vars)
-	_generate_graph_substitution()
+	invars, outvars = variables[:2], variables[2:]
+	_generate_substitution_test(invars, outvars)
+	_generate_relation_substitution(invars, outvars)
+	_generate_graph_substitution(invars, outvars)
 
 def evolve_graph(initial_graph, max_steps):
 	parse_rule()
